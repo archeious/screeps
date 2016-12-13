c407243434481536012eee5a6085e11802130b50
archeious <github@unbiasedgeek.com>
2016-12-13 13:56:32 -0700

    tower repais damaged buildings

--- main.js
+++ main.js
@@ -20,8 +20,9 @@ module.exports.loop = function () {
     
     var tower = Game.getObjectById('58505a22f89998f143bae36f'); 
     if(tower) { 
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, 
-            { filter: (structure) => structure.hits < [10000] }); 
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, { 
+            filter: (structure) => { return structure.hits < structures.hitsMax; }
+        }); 
         if(closestDamagedStructure) { tower.repair(closestDamagedStructure); }
         var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         if(closestHostile) {
