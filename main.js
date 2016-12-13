d6c2378f204032768e9f05b29dd8a1d1efc2d65b
archeious <github@unbiasedgeek.com>
2016-12-13 14:24:04 -0700

    t

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ module.exports.loop = function () {
         var roomStructures = Game.rooms[config.homeRoom].find(FIND_STRUCTURES, {filter:function(st){
                 return st.my && st.hits < st.hitsMax;
             }});
-        Game.Memory.debug = roomStructures;
+        tower.memory.debug = roomStructures;
         var closestDamagedStructure = roomStructures[0];
         if(closestDamagedStructure) { console.log("Repairing:" + tower.repair(closestDamagedStructure)); }
         var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
